7b0e5f4af580a96b4ab6e3ef7496abda78fa8c75
natxio <natxio@nglab.es>
2026-07-23 10:39:27 +0200

    fix: force SW activation on new release
    Trigger skipWaiting and clients.claim so installed PWA users immediately get the v3 HTML/JS and the ITV alert becomes visible without manual DevTools work.

--- public/sw.js
+++ public/sw.js
@@ -11,18 +11,17 @@ const STATIC_ASSETS = [
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(STATIC_ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
   );
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((names) =>
-      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
-    )
+    caches.keys()
+      .then((names) => Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n))))
+      .then(() => self.clients.claim())
   );
 });
 
